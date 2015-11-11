e81657afcb692b78e44b144a1825d0170c5f2606
=Nikhil <=nikhilmaheshwari123@gmail.com>
2015-11-11 13:10:33 +0530

    first commit

---
+++ js/_vti_cnf/slider.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|SKD-PC\\SKD
+vti_modifiedby:SR|SKD-PC\\SKD
+vti_timelastmodified:TR|11 Nov 2015 04:41:09 -0000
+vti_timecreated:TR|11 Nov 2015 04:28:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|11 Nov 2015 04:40:23 -0000
+vti_cacheddtm:TX|11 Nov 2015 04:41:09 -0000
+vti_filesize:IR|1248
